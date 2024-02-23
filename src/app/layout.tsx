4826cb6f19b9580d7ad6c1ab720f196c2c5487c6
Hadisa <afg.norozi@gmail.com>
2024-02-23 23:20:19 +0430

    new code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { type Metadata } from "next";
 import { Inter } from "next/font/google";
 import { type ReactNode } from "react";
 import "./globals.css";
-import Wrapper from "./wrapper";
+import Wrapper from "@/ui/components/wrapper";
 
 
 const inter = Inter({ subsets: ["latin"] });
