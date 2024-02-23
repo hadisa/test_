efbff34e03ea77d81da7728511e39915af5b2308
Hadisa <afg.norozi@gmail.com>
2024-02-23 23:14:12 +0430

    new code

--- src/app/[channel]/(main)/layout.tsx
+++ src/app/[channel]/(main)/layout.tsx
@@ -10,15 +10,11 @@ export const metadata = {
 export default function RootLayout(props: { children: ReactNode; params: { channel: string } }) {
 	return (
 		<>
-
-
-
 			<Header channel={props.params.channel} />
 			<div className="flex min-h-[calc(100dvh-64px)] flex-col">
 				<main className="flex-1">{props.children}</main>
 				<Footer channel={props.params.channel} />
 			</div>
-
 		</>
 	);
 }
